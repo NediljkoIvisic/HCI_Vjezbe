11e22b4d5f0e23cf9d73fb023ffdf59616d1c043
NediljkoIvisic <nediljko.ivisic99@gmail.com>
2020-11-03 20:26:13 +0100

    Creating PageContent

---
+++ src/components/ImageParagraph/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import styles from './style.module.css'
+ 
+const ImageParagraph = () => <section className={styles.imageParagraph}>
+
+</section> 
+ 
+export default ImageParagraph
