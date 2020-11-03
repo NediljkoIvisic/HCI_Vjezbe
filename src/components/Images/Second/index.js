845c135af844febf4b706aeda85dc6d17132da76
NediljkoIvisic <nediljko.ivisic99@gmail.com>
2020-11-03 21:37:15 +0100

    Creating ImageParagraph

---
+++ src/components/Images/Second/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+ 
+const Second = () => {
+    const data = useStaticQuery(graphql`
+    query {
+        myImage: file(relativePath: { eq: "drugaSlika.jpg" }) {
+            childImageSharp {
+                fluid(maxWidth: 800) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+    }
+    `)
+    return (
+        <div style={{maxWidth: "800px", minWidth: "300px"}}> 
+            <Img fluid={data.myImage.childImageSharp.fluid} />
+        </div>
+    )
+}
+ 
+export default Second
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
 import TitlePage from '../modules/TitlePage'
 import PageContent from '../modules/PageContent'
+import First from '../components/Images/First'
+import Second from '../components/Images/Second'
  
 const IndexPage = () => (
   <HeaderFooterLayout activeTab="Home">
