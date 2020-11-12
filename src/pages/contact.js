0583bac646a3a63caedfacc96036dc009349963f
NediljkoIvisic <nediljko.ivisic99@gmail.com>
2020-11-12 14:17:16 +0100

    Refactoring ContactSeperator

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,11 @@
 import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
-import ContactSeparator from '../modules/ContactSeperator'
+import SeperatorBar from '../modules/SeperatorBar'
 import ContactForm from '../modules/ContactForm'
  
 const ContactPage = () => (
     <HeaderFooterLayout activeTab="Contact">
-        <ContactSeparator />
+        <SeperatorBar text="Contact"/>
         <ContactForm />
     </HeaderFooterLayout>
 )
